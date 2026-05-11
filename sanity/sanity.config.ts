8150106cdef200c32e52a0364137efc9db24a657
Esteban M <esteban.montero@gmail.com>
2026-05-10 22:02:28 -0600

    Fix Sanity Studio preview URL env

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -19,7 +19,10 @@ export default defineConfig({
     visionTool(),
     presentationTool({
       previewUrl: {
-        origin: envValue(process.env.SANITY_STUDIO_PREVIEW_URL) ?? 'http://localhost:3000',
+        origin:
+          envValue(process.env.NEXT_PUBLIC_SANITY_STUDIO_PREVIEW_URL) ??
+          envValue(process.env.SANITY_STUDIO_PREVIEW_URL) ??
+          'http://localhost:3000',
         previewMode: {
           enable: '/api/draft-mode/enable',
         },
